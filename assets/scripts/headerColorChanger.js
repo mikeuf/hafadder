843d7e1d69a28670b2d4fa5e3e102e22d9f32c09
Mike <mblack101@ufl.edu>
2018-12-12 18:34:04 +0000

    added separate style sheet for splash page

--- assets/scripts/headerColorChanger.js
+++ assets/scripts/headerColorChanger.js
@@ -1,10 +1,14 @@
-$(function() {
-  'use strict';
-$('#logo').attr('src', 'images/haf-adder-logo-white.png');
-$('#header-container').css('background-color', '#ea4335');
-$('header').css('background-color', 'inherit');
-$('#header-desktop-nav').find('a').css('color', '#ffffff');
-$('footer').css('background-color', '#ea4335');
-$('#btn-hamburger').css('color','#ffffff');
+(function () {
+  var link  = document.createElement('link');
+  link.rel  = 'stylesheet';
+  link.type = 'text/css';
+  link.href = 'css/splash.css';
+  link.media = 'all';
+  document.head.appendChild(link);
 
-});
+  var logo = document.getElementById('logo');
+  logo.src='images/haf-adder-logo-color.png';
+
+}) ()
+
+
