d7b9db729f9ce7acefe2b303da02944c880f8489
Mike <mblack101@ufl.edu>
2018-12-12 04:54:59 +0000

    adjusting colors

---
+++ assets/scripts/headerColorChanger.js
@@ -0,0 +1,10 @@
+$(function() {
+  'use strict';
+$('#logo').attr('src', 'images/haf-adder-logo-white.png');
+$('#header-container').css('background-color', '#ea4335');
+$('header').css('background-color', 'inherit');
+$('#header-desktop-nav').find('a').css('color', '#ffffff');
+$('footer').css('background-color', '#ea4335');
+$('#btn-hamburger').css('color','#ffffff');
+
+});
